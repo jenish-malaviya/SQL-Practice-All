c5f4af01b3068c33c320cf0e37ab9f580e05bb7e
Jenish Malaviya <jenishmalaviya032@gmail.com>
2026-07-31 23:53:47 +0530

    Update Create Views 01.sql

--- views/Create Views 01.sql
+++ views/Create Views 01.sql
@@ -1,4 +1,5 @@
 SELECT * FROM Courses;
+SELECT * FROM Enrollments;
 
 -- Syntax
 
@@ -7,7 +8,7 @@ SELECT * FROM Courses;
 -- FROM table_name
 -- WHERE condition;
 
--- Create a View
+-- 3 Create a View
 
 CREATE VIEW information AS
 SELECT 
@@ -19,3 +20,48 @@ FROM Courses;
 
 SELECT *
 FROM information;
+
+-- 4 Where
+
+CREATE VIEW time_duration AS
+SELECT coursename,durationhours
+FROM Courses
+WHERE durationhours > 50;
+
+SELECT *
+FROM time_duration;
+
+-- Joins
+
+CREATE VIEW same_value AS
+SELECT c.courseid,c.coursename,c.category,e.studentname
+FROM Courses c
+INNER JOIN Enrollments e
+ON c.courseid = e.courseid;
+
+SELECT *
+FROM same_value;
+
+-- Aggregate Functions
+
+CREATE VIEW values_number AS
+SELECT 
+	AVG(price) AS Avg_Price,
+	MIN(price) AS Min_price,
+	MAX(price) AS Max_price,
+	SUM(price) AS Total_Sum,
+	COUNT(price) AS Total
+FROM Courses;
+
+SELECT *
+FROM values_number;
+
+-- Group by
+
+CREATE VIEW Find_avg_value AS
+SELECT courseid, AVG(amountpaid)
+FROM Enrollments
+GROUP BY courseid;
+
+SELECT *
+FROM Find_avg_value;
