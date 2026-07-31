4cd3f8b92cb4746817afe9cf9a394ea8f2bb04e9
Jenish Malaviya <jenishmalaviya032@gmail.com>
2026-07-31 16:47:55 +0530

    Add files via upload

---
+++ Subqueries and CTEs/Subquery_Advanced.sql
@@ -0,0 +1,15 @@
+SELECT * FROM Patients;
+SELECT * FROM Doctors;
+SELECT * FROM Appointments;
+SELECT * FROM Bills;
+
+-- Advanced (15–20)
+-- 15.Find the second highest doctor salary using a subquery.
+-- 16.Find doctors earning more than every doctor in the General department using ALL.
+-- 17. Find patients whose bill amount is greater than all pending bill amounts.
+-- 18. Find patients who visited Cardiology doctors.
+-- 19.Find the doctor who treated the highest number of unique patients.
+-- 20.Find the patient who generated the highest total bill using nested subqueries.
+
+-- 15.Find the second highest doctor salary using a subquery.
+
