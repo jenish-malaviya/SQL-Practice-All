16fc70f39d5ae7b6469f8f5a8abd2e6f2f0ccc3f
Jenish Malaviya <jenishmalaviya032@gmail.com>
2026-08-01 23:43:41 +0530

    Update Create Views 01.sql

--- views/Create Views 01.sql
+++ views/Create Views 01.sql
@@ -65,3 +65,120 @@ GROUP BY courseid;
 
 SELECT *
 FROM Find_avg_value;
+
+-- Having
+
+CREATE VIEW Total AS
+SELECT coursename,COUNT(price) AS Total
+FROM Courses
+GROUP BY coursename
+HAVING SUM(price) > 6000;
+
+SELECT * 
+FROM Total;
+
+-- Case
+
+CREATE VIEW Showing_price AS
+SELECT e.studentname,c.level,c.price,
+	CASE 
+		WHEN c.price > 6000 THEN 'High'
+		WHEN c.price > 3000 THEN 'Mediam'
+		ELSE 'Low'
+	END AS Compare
+FROM Enrollments e
+JOIN Courses c
+ON e.courseid = c.courseid;
+
+SELECT *
+FROM Showing_price
+WHERE compare = 'Low';
+
+-- Calculated Column
+
+CREATE VIEW Discount AS
+SELECT 
+		coursename,
+		category,
+		price,
+		price - (price * discountpercent / 100.0) AS price_afte_discount
+FROM Courses;
+
+SELECT *
+FROM Discount;
+
+-- Subsquries
+
+CREATE VIEW Low AS
+SELECT 
+		coursename,
+		category,
+		level,
+		price
+		FROM Courses
+		WHERE price IN (
+				SELECT price
+				FROM Courses
+				WHERE price < 4000
+		
+);
+
+SELECT *
+FROM Low;
+
+-- DISTINCT
+
+CREATE VIEW Group_by AS
+SELECT DISTINCT(language) AS category , language
+FROM Courses;
+
+SELECT *
+FROM Group_by;
+
+-- Nested View
+
+CREATE VIEW Active_Courses AS
+SELECT
+    CourseID,
+    CourseName,
+    Category,
+    InstructorName,
+    Level,
+    Price,
+    Rating,
+    CourseStatus
+FROM Courses
+WHERE CourseStatus = 'Active';
+
+CREATE VIEW High_Rated_Courses AS
+SELECT
+    CourseID,
+    CourseName,
+    Category,
+    InstructorName,
+    Price,
+    Rating
+FROM Active_Courses
+WHERE Rating >= 4.8;
+
+SELECT *
+FROM Active_Courses;
+
+SELECT *
+FROM High_Rated_Courses;
+
+-- Query in View
+
+CREATE VIEW Highest_amount AS 
+SELECT 
+		courseid, 
+		studentname,
+		paymentstatus,
+		amountpaid
+FROM Enrollments;
+
+SELECT *
+FROM Highest_amount
+WHERE amountpaid > 5000
+ORDER BY amountpaid 
+DESC;
